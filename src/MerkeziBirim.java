659043c1c8523f6d7a2cad0853d8b9724f95852f
Ferdi Sönmez <36517990+ferdisonmez@users.noreply.github.com>
2022-05-11 15:16:18 +0300

    Update MerkeziBirim.java

--- src/MerkeziBirim.java
+++ src/MerkeziBirim.java
@@ -2,17 +2,25 @@
 public class MerkeziBirim {
 
         private int sicaklik;
+        private boolean sogutucuAcik;
         Eyleyici eyleyici = new Eyleyici();
         SicaklikAlgilayici algilayici=new SicaklikAlgilayici();
-
-        public MerkeziBirim() {
-
+        public MerkeziBirim(boolean sogutucuAcik) {
+            this.sogutucuAcik=sogutucuAcik;
         }
+
         public int SicaklikGetir() {
             sicaklik = algilayici.sicaklikoku();
             return sicaklik;
         }
         public void SogutucuAc() {
+
+            if(this.sogutucuAcik==true){
+                System.out.println("Sogutucu zaten acik!");
+                return;
+            }
+            else {this.sogutucuAcik=true;}
+
             System.out.println("Soğutucu açılıyor.");
             try {
                 Thread.sleep(500);
@@ -25,6 +33,13 @@ public class MerkeziBirim {
         }
 
         public void SogutucuKapa() {
+
+             if(this.sogutucuAcik==false){
+                 System.out.println("Sogutucu zaten kapali!");
+                 return;
+             }
+             else {this.sogutucuAcik=false;}
+
             System.out.println("Soğutucu kapatılıyor.");
             try {
                 Thread.sleep(500);
@@ -34,5 +49,4 @@ public class MerkeziBirim {
             System.out.println("Soğutucu kapatıldı.");
             eyleyici.sogutucuKapa(sicaklik);
         }
-
 }
