5f4a86cfd2b39ad0c890854234e816dc77e0221c
Berat Özdin <32714982+beratozdin@users.noreply.github.com>
2022-05-11 15:13:22 +0300

    Update Arayuz.java

--- src/Arayuz.java
+++ src/Arayuz.java
@@ -1,6 +1,9 @@
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Arayuz {
 
+    //Kullanicinin basariyla girip girmedigini döndürür.
     public static boolean kullanicigonder(String user, String pass, UserKontrol control) {
         if(control.UsernameControl(user)>=0 && control.PasswordControl(pass,control.UsernameControl(user))) {
             return true;
@@ -9,6 +12,7 @@ public class Arayuz {
             return false;
     }
 
+    //Merkezi birimin görevlerini yaptigi kisim.
     public static void Islem(MerkeziBirim m) {
 
         Scanner sc = new Scanner(System.in);
@@ -30,43 +34,185 @@ public class Arayuz {
                 case 3:
                     m.SogutucuKapa();
                     break;
-
             }
 
         }while(secim != 4);
     }
 
-    public void arayuzBaslat() {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         UserKontrol control = new UserKontrol();
-        control.DatabaseUsers();
+        control.DatabaseUsers();//Veritabanindan kullanicilari okur.
+		MerkeziBirim ms = new MerkeziBirim(false);//Merkezi birim sogutucu kapali baslatilir.
 
-		MerkeziBirim ms = new MerkeziBirim();
+		int basarisizGiris = 0;
+		boolean girisBasarili = false;
 
-		int sayac = 0;
-		boolean temp = false;
-		while(sayac<3) {
+        //Ana menu
+        while (true) {
+            System.out.println("*******  Menu  ********\n" +
+                    "1 - Kullanici Girisi\n" +
+                    "2 - Kullanıcı olustur\n" +
+                    "3 - Kullanıcılara Mesaj Gönder:\n"+
+                    "4 - Cikis\n");
+            Scanner scanner = new Scanner(System.in);
+            int islem = scanner.nextInt();
+            //Islem=1 merkezi birimin menusune gider.
+            if (islem == 1) {
 
-			System.out.println("Username: ");
-			String user = sc.next();
-			System.out.println("Password: ");
-			String pass = sc.next();
+                while(basarisizGiris<3) {
 
-			if(kullanicigonder(user,pass,control)) {
-				temp = true;
+                    System.out.println("Kullanici Adi: ");
+                    String user = sc.next();
+                    System.out.println("Parola: ");
+                    String pass = sc.next();
 
-				break;
-			}
-			sayac++;
-		}
-
-		if(temp)
-			Islem(ms);
-		else
-			System.out.println("Your right of entry has expired");
-
+                    if(kullanicigonder(user,pass,control)) {
+                        girisBasarili = true;
+                        break;
+                    }
+                    basarisizGiris++;
+                }
+                if(girisBasarili){Islem(ms);}
+                else {System.out.println("3 Kere Basarisiz Kullanıcı Giris.");}
+                //Islem=2 kullanici ekler.
+            } else if (islem == 2) {
+                while (true) {
+                    if (KullaniciOlustur() == 3) {
+                        break;
+                    }
+                }
+                //Islem=3 Veritabanindaki tum kullanicilara mesaj gonderir.
+            } else if (islem == 3) {
+                sendStateMachineMessage();
+            } else {
+                System.out.println("Sistem sonlandiriliyor.");
+                break;
+            }
+        }
 	}
 
+    public static int KullaniciOlustur(){
+
+
+        String query="INSERT INTO public.users(name, password, usertype)"+ "VALUES (?, ?, ?)";
+        DatabaseConnection instance = DatabaseConnection.getInstance();
+        Connection conn = DatabaseConnection.getConnection();
+
+        UserCreating userCreating1=new UserCreating(new AdminUserFactory());
+        UserCreating userCreating2=new UserCreating(new BasicUserFactory());
+
+        IUsers iUser;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Kullanıcı türünü giriniz: 1-Admin 2-Basic User 3- Ana Menü");
+        int type=scanner.nextInt();
+        if(type==1){
+            scanner.nextLine();
+            System.out.println("Kullanici adinizi giriniz:");
+            String name=scanner.nextLine();
+            System.out.println("Parolanizi giriniz:");
+            String password=scanner.nextLine();
+            String usertype="admin";
+
+            //Factory deseni ile yeni bir kullanici olusturulur.
+            iUser=userCreating1.initUser(name,password);
+            name=iUser.getName();
+            password=iUser.getPassword();
+
+            try{
+                PreparedStatement pstmt = conn.prepareStatement(query);
+
+                pstmt.setString(1,name);
+                pstmt.setString(2,password);
+                pstmt.setString(3, "admin");
+                pstmt.executeUpdate();
+
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+            try {
+                conn.close();
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return type;
+
+        }
+        else if(type==2){
+            scanner.nextLine();
+            System.out.println("Kullanici adinizi giriniz:");
+            String name=scanner.nextLine();
+            System.out.println("Parolanizi giriniz:");
+            String password=scanner.nextLine();
+            String usertype="user";
+
+            //Factory deseni ile yeni bir kullanici olusturulur.
+            iUser=userCreating2.initUser(name,password);
+            name=iUser.getName();
+            password=iUser.getPassword();
+
+            try{
+                PreparedStatement pstmt = conn.prepareStatement(query);
+
+                pstmt.setString(1,name);
+                pstmt.setString(2,password);
+                pstmt.setString(3, "user");
+                pstmt.executeUpdate();
+
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+            try {
+                conn.close();
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return type;
+        }
+        else {
+            try {
+                conn.close();
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return 3;
+        }
+    }
+    public static void sendStateMachineMessage(){
+
+        ArrayList<IObserver> iUsers=new ArrayList<>();
+        DatabaseConnection instance = DatabaseConnection.getInstance();
+        Connection conn = DatabaseConnection.getConnection();
+
+        //Tum kullanicilar veritabanından okunur.
+        String query="SELECT *" + "FROM public.users";
+        try {
+
+            Statement qr=conn.createStatement();
+            ResultSet rs=qr.executeQuery(query);
+            while (rs.next()){
+                if(rs.getString("usertype").equals("admin")){
+
+                    iUsers.add(new AdminUser(rs.getString("name"), rs.getString("password"),"admin"));
+                }
+                else {
+                    iUsers.add(new BasicUser(rs.getString("name"), rs.getString("password"),"user"));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        //Observer deseni ile tum kullanicilara mesaj gönderme
+        Publisher publisher=new Publisher(iUsers);
+        publisher.notifying("Akıllı Cihaz Arızalandı.");
+
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
